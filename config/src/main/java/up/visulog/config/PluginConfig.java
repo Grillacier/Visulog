5ee033c0a15b8a7d5834fa3d4d03d3a31466a7cd
CHHAY davy <davychhay14@gmail.com>
2020-10-23 22:30:06 +0200

    Update PluginConfig.java

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,5 +1,5 @@
 package up.visulog.config;
-
+// Ajout de fonctionnalité pour paramétrer la page
 // TODO: define what this type should be (probably a Map: settingKey -> settingValue)
 public interface PluginConfig {
 }
