0f58323d18dd1b6c85c47fe04d1b77d36a8b8c90
Alaia <alaia98@gmail.com>
2020-11-05 21:36:03 +0100

    Revert "run plugins in parallel"
    This reverts commit 4684db4be4bbe7b84c57b3314aa8c880e046bda1.

--- analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
+++ analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
@@ -8,35 +8,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class Analyzer implements Runnable {
+public class Analyzer {
     private final Configuration config;
 
     private AnalyzerResult result;
 
     public Analyzer(Configuration config) {
         this.config = config;
-    }
-
-    //creation of a Thread to stock the runnable plugins
-    private Thread tousLesPlugins = new Thread();
-
-    private volatile boolean running = true;
-
-    public void arreter() {
-        this.running = false;
-    }
-
-    //run() method from the Runnable interface
-    @Override
-    public void run() {
-        while (running) {
-            //traitement du thread
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
     }
 
     public AnalyzerResult computeResults() {
@@ -48,12 +26,8 @@ public class Analyzer implements Runnable {
             plugin.ifPresent(plugins::add);
         }
         // run all the plugins
-        for (var plugin: plugins) {
-            tousLesPlugins = Thread(plugin);
-        }
-        //start() calls the run() method
-        tousLesPlugins.start();
-        arreter();
+        // TODO: try running them in parallel
+        for (var plugin: plugins) plugin.run();
 
         // store the results together in an AnalyzerResult instance and return it
         return new AnalyzerResult(plugins.stream().map(AnalyzerPlugin::getResult).collect(Collectors.toList()));
