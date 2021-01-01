b245ab0cd2a64400c64aaf91c6a61f08396ff9a5
CHHAY davy <davychhay14@gmail.com>
2021-01-01 23:40:12 +0100

    Add JavaDoc

--- config/src/main/java/up/visulog/config/Configuration.java
+++ config/src/main/java/up/visulog/config/Configuration.java
@@ -10,15 +10,28 @@ public class Configuration {
     private final Path gitPath;
     private final Map<String, PluginConfig> plugins;
 
+    /**
+      *Constructor of configuration
+      *
+      *@param gitPath Path of the git that will be printed
+      *@param plugins List of plugin that will be added
+    */
     public Configuration(Path gitPath, Map<String, PluginConfig> plugins) {
         this.gitPath = gitPath;
         this.plugins = Map.copyOf(plugins);
     }
+    /** Getter of getGitPath
+      *
+      *@return The actual gitPath
 
+    */
     public Path getGitPath() {
         return gitPath;
     }
-
+    /**The getter of plugins
+      *
+      *@return The actual plugins in this coniguration
+      */
     public Map<String, PluginConfig> getPluginConfigs() {
         return plugins;
     }
