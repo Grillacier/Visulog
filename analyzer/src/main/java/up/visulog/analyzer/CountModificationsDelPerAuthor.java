f81492ac23800c37febd6c7e38bfe6569dd9f1d6
CHHAY davy <davychhay14@gmail.com>
2021-01-01 23:36:54 +0100

    Add JavaDoc

--- analyzer/src/main/java/up/visulog/analyzer/CountModificationsDelPerAuthor.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountModificationsDelPerAuthor.java
@@ -14,7 +14,13 @@ public class CountModificationsDelPerAuthor implements AnalyzerPlugin{
     public CountModificationsDelPerAuthor(Configuration generalConfiguration) {
         this.configuration = generalConfiguration;
     }
-
+    /**
+    * Count the number of modification deleted of the list
+    *
+    *@param gitLog List of commits
+    *
+    *@return Number of modification delete
+    */
     static Result processLog(List<Commit> gitLog) {
         var result = new Result();
         for (var commit : gitLog) {
@@ -27,36 +33,67 @@ public class CountModificationsDelPerAuthor implements AnalyzerPlugin{
     }
 
     @Override
+    /**
+    *Set the current result
+    *
+    *@see processLog
+    */
 	public void run() {
         //System.out.println(result+"aaaaaaaaaaaaaaaaaaa");
         result = processLog(Commit.parseLogFromCommand(configuration.getGitPath()));
 	}
 
 	@Override
+  /**
+    * Set result with the precessLog's fonction if result is not defined or return the current result
+    *
+    *@return The actual result
+    *
+    *@see run
+  */
 	public Result getResult() {
         if (result == null) run();
         return result;
 	}
 
-	
+
 	public static class Result implements AnalyzerPlugin.Result {
         private final Map<String, Integer> modificationPerAuthor = new HashMap<>();
 
+        /**
+          *Getter of modificationPerAuthor
+          *
+          *@return The current modificationPerAuthor
+        */
         public Map<String, Integer> getCommitsPerAuthor() {
             return modificationPerAuthor;
         }
-
+        /**
+          *Give the String to modification per author and return it;
+          *
+          *@return modificationPerAuthor toString return
+        */
         @Override
         public String getResultAsString() {
             return modificationPerAuthor.toString();
         }
-        
+        /**
+         *Create an html division
+         *
+         *@return html code
+
+       */
         @Override
         public String getResultAsHtmlDiv() {
         	StringBuilder html = new StringBuilder("<h1 style='text-align: center;'> Count Modification Per Author </h1>");
             return html.toString();
         }
-        
+        /**
+         *return the result composed of value of the hashmap
+         *
+         *@return list of value of modificationPerAuthor
+
+       */
         @Override
         public String getResultAsStringValue() {
         	String parse="";
@@ -65,7 +102,11 @@ public class CountModificationsDelPerAuthor implements AnalyzerPlugin{
             }
             return parse;
         }
-        
+        /**
+       *return a list of key as an String of the hashmap modificationPerAthor
+       *
+       *@return a String composed of keys
+     */
         public String getResultAsStringKey() {
         	String parse="";
             for (var item : modificationPerAuthor.entrySet()) {
