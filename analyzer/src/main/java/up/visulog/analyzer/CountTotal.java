0568282adf693dc0b582788cdf288b01fd3b9755
CHHAY davy <davychhay14@gmail.com>
2021-01-02 02:28:15 +0100

    Revert "Add JavaDoc"
    This reverts commit 07d12d44ce58113087c36097e113444ddd46a2d7

--- analyzer/src/main/java/up/visulog/analyzer/CountTotal.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountTotal.java
@@ -27,24 +27,12 @@ public class CountTotal implements AnalyzerPlugin {
     }
 
     @Override
-    /**
-    *Set the current result
-    *
-    *@see processLog
-    */
     public void run() {
         //System.out.println(result+"aaaaaaaaaaaaaaaaaaa");
         result = processLog(Commit.parseLogFromCommand(configuration.getGitPath()));
     }
 
     @Override
-    /**
-      * Set result with the precessLog's fonction if result is not defined or return the current result
-      *
-      *@return The actual result
-      *
-      *@see run
-    */
     public Result getResult() {
         if (result == null) run();
         return result;
@@ -57,32 +45,18 @@ public class CountTotal implements AnalyzerPlugin {
         public Map<String, Integer> getCommitsPerAuthor() {
             return modificationPerAuthor;
         }
-        /**
-          *Give the String to modification per author and return it;
-          *
-          *@return modificationPerAuthor toString return
-        */
+
         @Override
         public String getResultAsString() {
             return modificationPerAuthor.toString();
         }
-        /**
-         *Create an html division
-         *
-         *@return html code
 
-       */
         @Override
         public String getResultAsHtmlDiv() {
             StringBuilder html = new StringBuilder("<h1 style='text-align: center;'> Count total number of Modifications Per Author </h1>");
             return html.toString();
         }
-        /**
-         *return the result composed of value of the hashmap
-         *
-         *@return list of value of modificationPerAuthor
 
-       */
         @Override
         public String getResultAsStringValue() {
             String parse="";
@@ -91,11 +65,7 @@ public class CountTotal implements AnalyzerPlugin {
             }
             return parse;
         }
-        /**
-       *return a list of key as an String of the hashmap modificationPerAthor
-       *
-       *@return a String composed of keys
-     */
+
         public String getResultAsStringKey() {
             String parse="";
             for (var item : modificationPerAuthor.entrySet()) {
